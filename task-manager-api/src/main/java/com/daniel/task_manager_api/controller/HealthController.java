1f2f9288c02b6c8ec55139fb3ae99395397e9406
Daniel <midiscord12345@gmail.com>
2026-07-06 13:30:16 +0200

    Add health check endpoint

---
+++ task-manager-api/src/main/java/com/daniel/task_manager_api/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.daniel.task_manager_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String checkHealth() {
+        return "Task Manager API is running ";
+    }
+
+}
